a9e02889a534f2231db98ecb7049a6b9ce4276d1
BartoszPerkowski27 <bartosz.perkowski@infotech.edu.pl>
2024-02-26 14:31:47 +0100

    klasa

--- game.py
+++ game.py
@@ -1,6 +1,43 @@
 import pygame
 
-pygame.init()
-res = 1280, 720
-pygame.display.set_mode(res)
-pygame.display.set_caption("Waaaaaaaagh")
+class App:
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.weight, self.height = 640, 400
+        self.title = "Śląskie Koty"
+
+    def on_init(self):
+        pygame.init()
+        pygame.display.set_caption(self.title)
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
+
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
+
+    def on_loop(self):
+        pass
+
+    def on_render(self):
+        pass
+
+    def on_cleanup(self):
+        pygame.quit()
+
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+
+        while (self._running):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+
+
+if __name__ == "__main__":
+    theApp = App()
+    theApp.on_execute()
